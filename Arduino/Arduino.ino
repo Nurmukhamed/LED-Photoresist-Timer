50a4b4a82b92c1132d7bbe7bee18a51b0cbe4782
Дмитрий Дементьев <info@phpscript.ru>
2018-02-13 12:43:33 +0300

    Вторая версия таймера и матрицы

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,49 +1,50 @@
+/*
+ LED засветка фоторезиста и паяльной маски
+ 
+ https://github.com/phpscriptru/LED-Photoresist-Timer
+*/
+
 #include <LiquidCrystal.h>
 #include <Bounce.h>
+#include <Encod_er.h>
 
-int PinPres1 = 5; // кнопки
-int PinPres2 = 6;
-int PinRelay = 13; // реле
-int PinCLK = 2; // энкодер
-int PinDT = 3;
-int PinSW = 4;
-long Preset0 = 0;  // начальное значение (сек.)
 long Preset1 = 20;  // предустановка времени №1 (сек.)
 long Preset2 = 140;  // предустановка времени №2 (сек.)
+long Preset3 = 240;  // предустановка времени №3 (сек.)
 int sec_step = 5; // шаг энкодера (сек.)
 
 unsigned long currentTime; // текущее время
 unsigned long loopTime; // время окончания
 unsigned long LimitTime = 0; // таймер
-volatile boolean turn;
-volatile boolean up;
-boolean running = false; // флаг запуска отсчета
-int awake1, awake2, awake3;
-
+boolean working = false; // флаг запуска отсчета
+int n1,n2,n3,n4,n5 = LOW;
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7); // экран
-Bounce bouncer1 = Bounce(PinPres1, 5); // дребезг кнопок
-Bounce bouncer2 = Bounce(PinPres2, 5); 
-Bounce bouncer3 = Bounce(PinSW, 5); 
+Encod_er encoder(A2, A1, 2); // энкодер
+Bounce bouncer1 = Bounce(6, 5); // дребезг кнопок
+Bounce bouncer2 = Bounce(5, 5); 
+Bounce bouncer3 = Bounce(4, 5);  
+Bounce bouncer4 = Bounce(A0, 5); 
 
 void setup() {
 
-  Preset0 = Preset0*1000;
   Preset1 = Preset1*1000;
   Preset2 = Preset2*1000;
+  Preset3 = Preset3*1000;
   sec_step = sec_step*1000;
-  LimitTime = Preset0;
-  pinMode(PinCLK, INPUT); // входы
-  pinMode(PinDT, INPUT);  
-  pinMode(PinSW, INPUT);
-  pinMode(PinPres1, INPUT);
-  pinMode(PinPres2, INPUT);
-  pinMode(PinRelay, OUTPUT);
-  attachInterrupt (0, isr, CHANGE); // прерывание энкодера
+  LimitTime = 0;
+  pinMode(A1, INPUT);// входы
+  pinMode(A2, INPUT);
+  pinMode(A0, INPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(13, OUTPUT);
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.print("  UV LED TIMER  ");
   lcd.setCursor(0,1);
-  lcd.print("    ver. 1.0    ");
+  lcd.print("    ver. 2.0    ");
   delay(1000);
   lcd.clear();
   currentTime = millis();
@@ -51,26 +52,25 @@ void setup() {
 
 void loop() {
 
-  delay(200);
-
   bouncer1.update(); // дребезг кнопок
   bouncer2.update();
   bouncer3.update();
+  bouncer4.update();
  
-  // выбран режим отсчета
-  /* ========================== */
-  if (running == true) {
+  /**
+   * Выбран режим отсчета
+   */
+  if (working == true) {
 
-    // отсчет времени
     currentTime = millis();
     loopTime = currentTime + LimitTime; // когда закончить
     while(currentTime < loopTime) { // сравниваем текущее время с временем окончания
 
-      digitalWrite(PinRelay, HIGH); // включаем
+      digitalWrite(13, HIGH); // включаем
 
       // вывод на экран
       lcd.setCursor(0,0);
-      lcd.print("   Working...   "); 
+      lcd.print("   Exposure...  "); 
       lcd.setCursor(4, 2);  
       if (LimitTime/60/60/1000 < 10) { lcd.print("0"); }
       lcd.print(LimitTime/60/60/1000);
@@ -82,15 +82,14 @@ void loop() {
       lcd.print(LimitTime/1000%60);
       lcd.display();
 
-      // кнопка
-      bouncer3.update();
-      if (bouncer3.read() != awake3) {
- 
-        awake3 = bouncer3.read();
-        if (awake3 && running == true) {
-          running = false;
-          digitalWrite(PinRelay, HIGH); // включаем
-          LimitTime = Preset0;
+      // кнопка энкодера
+      bouncer4.update();
+      if (bouncer4.read() != n4) {
+         n4 = bouncer4.read();
+        if (n4 && working == true) {
+          working = false;
+          digitalWrite(13, HIGH); // включаем
+          LimitTime = 0;
           break;
         }
       }
@@ -99,28 +98,25 @@ void loop() {
       currentTime = millis(); // получаем новое время
     }
 
-    // окончание работы таймера 
-    digitalWrite(PinRelay, LOW); // отключаем устройство
+    digitalWrite(13, LOW); // отключаем 
 
-    lcd.setCursor(0, 0);
-    lcd.print("             ");
-    lcd.setCursor(0, 0);
+    lcd.clear();
     lcd.print("     FINISH     ");
-    running = false;
+    working = false;
     delay (3000);
-    //lcd.begin(16, 2);
 
-  // выбран режим клавиатуры
-  /* ========================== */
+  /**
+   * Выбран режим клавиатуры
+   */
   } else {
 
-    digitalWrite(PinRelay, LOW); // отключаем устройство  
+    digitalWrite(13, LOW); // отключаем  
     
-    if (LimitTime > 86400000) LimitTime = Preset0; // отбрасываем некорректные значения
-    
+    if (LimitTime > 86400000) LimitTime = 0; // отбрасываем некорректные значения
+
     // вывод на экран
     lcd.setCursor(0,0);
-    lcd.print("   Set timer:   "); 
+    lcd.print("   Set timer:   ");
     lcd.setCursor(4, 2);  
     if (LimitTime/60/60/1000 < 10) { lcd.print("0"); }
     lcd.print(LimitTime/60/60/1000);
@@ -130,12 +126,11 @@ void loop() {
     lcd.print(":");
     if (LimitTime/1000%60 < 10) { lcd.print("0"); }
     lcd.print(LimitTime/1000%60);
-    lcd.display();
 
     // пресет №1
-    if (bouncer1.read() != awake1) {
-      awake1 = bouncer1.read();
-      if (awake1) {
+    if (bouncer1.read() != n1) {
+      n1 = bouncer1.read();
+      if (n1) {
         lcd.setCursor(0,1);
         lcd.print("    preset 1    ");
         LimitTime = Preset1;
@@ -144,9 +139,9 @@ void loop() {
     }
 
     // пресет №2
-    if (bouncer2.read() != awake2) {
-      awake2 = bouncer2.read();
-      if (awake2) {
+    if (bouncer2.read() != n2) {
+      n2 = bouncer2.read();
+      if (n2) {
         lcd.setCursor(0,1);
         lcd.print("    preset 2    ");
         LimitTime = Preset2;
@@ -154,33 +149,36 @@ void loop() {
       }
     }
 
-    // энкодер
-    static long enc_pos=0;
-    if (digitalRead(PinSW) && enc_pos != 0) {
-      enc_pos=0;
-    }
-    if (turn) {
-      if (up) {
-        enc_pos--;
-      } else {
-        enc_pos++;
+    // пресет №3
+    if (bouncer3.read() != n3) {
+      n3 = bouncer3.read();
+      if (n3) {
+        lcd.setCursor(0,1);
+        lcd.print("    preset 3    ");
+        LimitTime = Preset3;
+        delay(1000);
       }
-      turn = false;
-      LimitTime += enc_pos*sec_step;
     }
 
-    // кнопка
-    if (bouncer3.read() != awake3) {
-      awake3 = bouncer3.read();
-      if (awake3 && LimitTime != 0) {
-        running = true;
+    // энкодер
+    encoder.scanState(); 
+    if(encoder.timeRight != 0) {
+      encoder.read();
+      encoder.timeRight= 0;
+      LimitTime += sec_step;
+    }
+    if(encoder.timeLeft != 0) {
+      encoder.read();
+      encoder.timeLeft= 0;
+      LimitTime -= sec_step;
+    } 
+    
+    // кнопка энкодера
+    if (bouncer4.read() != n4) {
+      n4 = bouncer4.read();
+      if (n4 && LimitTime != 0) {
+        working = true;
       }
     }
   }
-}
-
-// прерывание энкодера
-void isr ()  {
-  up = (digitalRead(PinCLK) == digitalRead(PinDT));
-  turn = true;
 }
--- README.md
+++ README.md
@@ -1,26 +1,13 @@
-# Матрица засветки фоторезиста LED светодиодами на Arduino
-Система засветки фоторезиста на Arduino
-
-#### Контроллер
-Arduino Nano с экраном 1602, энкодер для ручного установки времени и 2 кнопки с предустановками (в скетче).
-
-#### Светодиоды
-* Диаметр: 5 мм
-* Длина волны: 395 ~ 400nm
-* Напряжение: 3.2 ~ 3.8v
-* Ток: 30mA
-
-Подключаются в цепочки по 3 штуки с резистором 52Ома.
-
+# LED засветка фоторезиста и паяльной маски
+Система засветки фоторезиста и паяльной маски на Arduino. Это вторая улучшенная версия. Отличается платами 10х10 см из которых можно собрать матрицу любой площади. 
 #### Youtube Видео
-[![LED Photoresist Timer](http://img.youtube.com/vi/yPS-ldzMQRI/0.jpg)](http://www.youtube.com/watch?v=yPS-ldzMQRI)
+[![LED-Photoresist-Timer](http://img.youtube.com/vi/Ps6n-hePVEI/0.jpg)](https://youtu.be/Ps6n-hePVEI)
+#### Ссылки на сопутствующие товары:
+1) https://goo.gl/NB2EyK Arduino NANO;
+2) https://goo.gl/b64vpc Светодиоды;
+3) https://goo.gl/gxeTg7 Экран;
+4) https://goo.gl/FU2FH8 Тестер;
 
-#### Где купить:
-* Светодиоды - http://ali.pub/wtn7r
-* Экран - http://ali.pub/j97ae
-* Энкодер - http://ali.pub/2uoc1
-* Arduino - http://ali.pub/2hrif
-
-Группа в VK: http://vk.com/hamradiogoods. Каждый день выкладываю полезные товары радиолюбителю
+Группа в VK: http://vk.com/hamradiogoods. Еще больше полезных товаров радиолюбителю
 
 По всем вопросам: info@phpscript.ru
